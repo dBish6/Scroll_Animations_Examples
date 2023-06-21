7a86f1895e6333e9ad059545289068b3a7f399c9
dBish6 <davidbish2002@hotmail.com>
2023-06-20 22:53:20 -0230

    Nicer Footer

--- src/components/partials/footer/index.tsx
+++ src/components/partials/footer/index.tsx
@@ -1,19 +1,20 @@
 import "./footer.css";
+import externalIcon from "../../../assets/icons/external-link.svg";
 
 const Footer = () => {
   return (
     <footer>
       <p>
-        Created By{" "}
+        Created By:{" "}
         <a
           href="https://github.com/dBish6"
           target="_blank"
           rel="noopener noreferrer"
         >
-          David Bishop
+          David Bishop <img src={externalIcon} alt="External" />
         </a>
       </p>
-      <p>Thanks to Open Peeps for the illustrations!</p>
+      <small>Special thanks to Open Peeps for the illustrations!</small>
     </footer>
   );
 };
