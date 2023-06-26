422e15aed48362b0b712dfde2f5b08dc23c31985
dBish6 <davidbish2002@hotmail.com>
2023-06-26 00:06:53 -0230

    Test

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -8,24 +8,25 @@ const Hero = ({ ToAnimate, animationType }: SectionProps) => {
 
   useEffect(() => {
     console.log("RUNNING");
-
-    const observer = new IntersectionObserver((entries) => {
-      console.log("entries", entries);
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("sAnimate");
-        } else {
-          entry.target.classList.remove("sAnimate");
-        }
+    if (animationType === "css") {
+      const observer = new IntersectionObserver((entries) => {
+        console.log("entries", entries);
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("sAnimate");
+          } else {
+            entry.target.classList.remove("sAnimate");
+          }
+        });
       });
-    });
 
-    document.querySelectorAll(".toAnimate").forEach((elem) => {
-      console.log("elem", elem);
-      observer.observe(elem);
-    });
+      document.querySelectorAll(".toAnimate").forEach((elem) => {
+        console.log("elem", elem);
+        observer.observe(elem);
+      });
 
-    return () => observer && observer.disconnect();
+      return () => observer && observer.disconnect();
+    }
   }, []);
 
   // useEffect(() => {
